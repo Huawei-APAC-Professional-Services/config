3a030791e43f9134c994fcf1e1b0ca9a7619d925
mu <agoodmu@gmail.com>
2023-11-13 16:20:26 +0800

    update code

--- function/go.mod
+++ function/go.mod
@@ -3,8 +3,20 @@ module main
 go 1.21.0
 
 require (
-	github.com/Huawei-APAC-Professional-Services/config-rules v0.0.0-20231111025400-8655e94643a1
+	github.com/Huawei-APAC-Professional-Services/config-rules v0.0.0-20231113081120-e23e20c771b5
 	huaweicloud.com/go-runtime v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/huaweicloud/huaweicloud-sdk-go-v3 v0.1.66 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
+	go.mongodb.org/mongo-driver v1.13.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 replace huaweicloud.com/go-runtime => ../go-runtime
--- function/main.go
+++ function/main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log/slog"
+	"reflect"
 
 	"github.com/Huawei-APAC-Professional-Services/config-rules/event"
+	"github.com/Huawei-APAC-Professional-Services/config-rules/service"
 	huaweicontext "huaweicloud.com/go-runtime/go-api/context"
 	huaweiruntime "huaweicloud.com/go-runtime/pkg/runtime"
 )
 
-func handler(event event.ConfigEvent, ctx huaweicontext.RuntimeContext) (interface{}, error) {
-	fmt.Println(event.DomainId)
-	return "test", nil
+const (
+	CheckOnlyOneEnterpriseAdministrator string = "one_enterprise_administrator"
+)
+
+var iamService *service.ConfigIAMClient
+
+func handler(payload []byte, ctx huaweicontext.RuntimeContext) error {
+	var et event.ConfigEvent
+	err := json.Unmarshal(payload, &et)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+	slog.Info(string(payload))
+	ak := ctx.GetAccessKey()
+	sk := ctx.GetSecretKey()
+	token := ctx.GetToken()
+	region := et.InvokingEvent.RegionId
+	switch provider := et.InvokingEvent.Provider; provider {
+	case "iam":
+		iamService = service.NewIAMClient(ak, sk, region)
+	default:
+		fmt.Println("test")
+	}
+	switch ruletype := et.RuleParameter["rule"].(type) {
+	case string:
+		fmt.Printf("String: %v\n", ruletype)
+	default:
+		fmt.Printf("Yes: %v\n", reflect.TypeOf(ruletype))
+	}
+	switch rule := et.RuleParameter["rule"].(string); rule {
+	case CheckOnlyOneEnterpriseAdministrator:
+		result, err := iamService.HasOnlyOneEnterpriseAdministrator()
+		if err != nil {
+			return err
+		}
+		if result {
+			return et.ReportComplianceStatus(event.CompliantResult, token)
+		} else {
+			return et.ReportComplianceStatus(event.NonCompliantResult, token)
+		}
+	default:
+		slog.Error("unkonw customized rule")
+		return errors.New("unknow customized rule")
+	}
 }
 
 func main() {
